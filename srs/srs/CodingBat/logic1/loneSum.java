f5aabf71bf0464e8e053c7c4fd1e9435c7e10769
Aizha <aizhant51@gmail.com>
2024-01-25 19:49:21 +0300

    codingbat -Logic LoneSum

--- srs/srs/CodingBat/logic1/loneSum.java
+++ srs/srs/CodingBat/logic1/loneSum.java
@@ -1,2 +1,12 @@
-package CodingBat.logic1;public class loneSum {
+package CodingBat.logic1;
+
+public class loneSum {
+    public static void main(String[] args) {
+
+    }
+    public int loneSum(int a, int b, int c) {
+      return b;
+    }
+
+
 }
