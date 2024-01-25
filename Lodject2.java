85206dbb55b95a4f8c8caa58a190f1a30830b355
Gulayim2024 <157340208+Gulayim2024@users.noreply.github.com>
2024-01-25 23:19:06 +0600

    Create Lodject2.java

---
+++ Lodject2.java
@@ -0,0 +1,25 @@
+
+
+public class makeBricks {
+    public static void main(String[] args) {
+                     //1+1+1+5=8
+        // makeBricks(3, 1, 8) → true
+        // makeBricks(3, 1, 9) → false
+        // makeBricks(3, 2, 10) → true
+  makeBricks  make=new makeBricks();
+        System.out.println(make.makeBricks(3, 1, 8));
+        System.out.println(make.makeBricks(3, 1, 9));
+        System.out.println(make.makeBricks(3, 2, 10));
+    }
+    public boolean makeBricks(int small, int big, int goal) {
+        //8   :5>= 1
+        if (goal > small + big * 5) {
+            return false;
+        }
+        int smallBricks = goal % 5;
+
+        return smallBricks <= small;
+    }
+
+
+}
