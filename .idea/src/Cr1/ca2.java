6fb9b619d6e8901883a811fb41f44f826c9d4ae6
Guli55 <gokombayeva@bk.ru>
2024-01-28 22:21:23 +0600

    C1-commit3

--- .idea/src/Cr1/ca2.java
+++ .idea/src/Cr1/ca2.java
@@ -3,5 +3,8 @@ package Cr1;
 public class ca2 {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+    }public static void sayHello(){
+        System.out.println("Hi Bem!");
     }
 }
