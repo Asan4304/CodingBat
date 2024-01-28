eebb9ee5fd9060961f0c0ca7ae0096f0f1d66637
Guli55 <gokombayeva@bk.ru>
2024-01-28 22:26:08 +0600

    Revert "C1-commit4563"
    This reverts commit 6fb9b619d6e8901883a811fb41f44f826c9d4ae6.

--- .idea/src/Cr1/ca2.java
+++ .idea/src/Cr1/ca2.java
@@ -3,8 +3,5 @@ package Cr1;
 public class ca2 {
     public static void main(String[] args) {
         System.out.println("Hello");
-
-    }public static void sayHello(){
-        System.out.println("Hi Bem!");
     }
 }
